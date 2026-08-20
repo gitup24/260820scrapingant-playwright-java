9ca35fcb5bca3b70aef0307fdeb824cc7fcee581
m e <duchatelle@gmail.com>
2026-08-20 21:29:20 +0200

    save to JSON

---
+++ src/main/java/org/example/main/Main.java
@@ -0,0 +1,53 @@
+package org.example.main;
+
+import java.util.List;
+
+public class Main {
+    static void main() {
+        //
+        System.out.println("Starting scraper...");
+        WebScraper scraper = new WebScraper();
+        System.out.println("Scraper initialized, calling scrapeData...");
+        try {
+            List<String> data = scraper.scrapeData("https://google.com", "h1");
+            System.out.println("Scraped data: " + data);
+            /*
+            DataProcessor processor = new DataProcessor();
+            processor.saveToJson(processor.cleanData(data), "/home/olivier/IdeaProjects/260820scrapingant-playwright-java/src/main/java/org/example/main/scraped_data.json");
+            */
+
+
+        } catch (Exception e) {
+            System.err.println("Exception occurred: " + e.getMessage());
+        } finally {
+            System.out.println("Closing scraper...");
+            scraper.close();
+            System.out.println("Done!");
+        }
+        //
+        System.out.println("Starting dynamicScraper...");
+        DynamicContentScraper dynamicScraper = new DynamicContentScraper();
+        System.out.println("Dynamic Scraper initialized, calling scrapeData...");
+        try {
+            List<String> dataDynamic = dynamicScraper.scrapeDynamicContent("https://google.com", "h1");
+            System.out.println("Dynamic scraped data: " + dataDynamic);
+
+            //save to JSON
+            DataProcessor processor = new DataProcessor();
+            processor.saveToJson(processor.cleanData(dataDynamic), "/home/olivier/IdeaProjects/260820scrapingant-playwright-java/src/main/java/org/example/main/scraped_data.json");
+            System.out.println("JSON saved successfully!");
+
+        } catch (Exception e) {
+            System.err.println("Exception occurred: " + e.getMessage());
+        } finally {
+            System.out.println("Closing dynamicScraper...");
+            dynamicScraper.close();
+            System.out.println("Done!");
+        }
+        /*
+
+        */
+
+
+    }
+}
